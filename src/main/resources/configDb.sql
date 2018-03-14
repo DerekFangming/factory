2b504182597bf64a6e62bb9756950664044bd268
Fangming Ning <synfm@126.com>
2018-03-14 07:29:10 -0400

    adding tables

--- src/main/resources/configDb.sql
+++ src/main/resources/configDb.sql
@@ -1,6 +1,35 @@
-create table about (
+create table companies (
 	id serial primary key,
-	test varchar(5),
-	number integer,
-	bool boolean
+	name text not null,
+	description text,
+	industry text,
+	licensed boolean not null default false,
+	created_at timestamp without time zone NOT NULL
+	updated_by integer
+);
+
+create table users (
+	id serial primary key,
+	username text not null,
+	password text not null,
+	access_token text,
+	verification_code text,
+	confirmed boolean not null default false,
+	salt text not null,
+	created_at timestamp without time zone NOT NULL,
+	role_id integer not null,
+	company_id integer not null,
+	registration_code integer,
+	updated_by integer,
+	name text not null,
+	phone text,
+	work_id text,
+	birthday timestamp without time zone,
+	joined_date timestamp without time zone
+);
+
+create table roles (
+	id serial primary key,
+	company_id integer not null,
+	
 );
