96ab25924d1d4af6a98c8b3c7ea5127203fb9853
synfm@126.com <synfm@126.com>
2018-04-01 22:53:13 -0400

    table changes

--- src/main/resources/configDb.sql
+++ src/main/resources/configDb.sql
@@ -4,7 +4,18 @@ create table companies (
 	description text,
 	industry text,
 	licensed boolean not null default false,
-	created_at timestamp without time zone NOT NULL
+	created_at timestamp without time zone NOT NULL,
+	owner_id integer not null,
+	updated_by integer
+);
+
+create table roles (
+	id serial primary key,
+	company_id integer not null REFERENCES companies,
+	name text not null,
+	level integer not null,
+	created_at timestamp without time zone NOT NULL,
+	owner_id integer not null,
 	updated_by integer
 );
 
@@ -17,7 +28,7 @@ create table users (
 	confirmed boolean not null default false,
 	salt text not null,
 	created_at timestamp without time zone NOT NULL,
-	role_level integer not null,
+	role_id integer not null REFERENCES roles,
 	company_id integer not null REFERENCES companies,
 	registration_code integer,
 	updated_by integer,
@@ -28,30 +39,40 @@ create table users (
 	joined_date timestamp without time zone
 );
 
-create table roles (
-	id serial primary key,
-	company_id integer not null REFERENCES companies,
-	name text not null,
-	level integer not null,
-	created_at timestamp without time zone NOT NULL,
-	owner_id integer not null REFERENCES users,
-	updated_by integer REFERENCES users
-);
-
 create table products (
 	id serial primary key,
 	company_id integer not null REFERENCES companies,
 	model text not null,
 	name text not null,
-	description text not null
+	description text,
+	combined_product boolean not null default false,
 	net_cost decimal,
 	market_price decimal,
-	price_visible_role_level integer,
+	price_visible_role_id integer REFERENCES roles,
 	labor_cost decimal,
-	
+	visible_role_id integer REFERENCES roles,
 	created_at timestamp without time zone NOT NULL,
 	owner_id integer not null REFERENCES users,
 	updated_by integer REFERENCES users
+);
+
+create table product_combinations (
+	id serial primary key,
+	parent_id integer not null REFERENCES products,
+	child_id integer not null REFERENCES products,
+	count integer not null,
+	step integer not null,
+	created_at timestamp without time zone NOT NULL,
+	owner_id integer not null REFERENCES users,
+	updated_by integer REFERENCES users
+);
+
+create table task_assignee_groups (
+	id serial primary key,
+	task_id integer REFERENCES tasks
+	assignee_id integer not null REFERENCES users,
+	created_at timestamp without time zone NOT NULL,
+	owner_id integer not null REFERENCES users
 );
 
 create table tasks (
@@ -61,7 +82,11 @@ create table tasks (
 	type text not null,
 	description text,
 	status text not null,
-	visible_role_level integer,
+	priority text not null,
+	visible_role_id integer REFERENCES roles,
+	assignee integer REFERENCES users,
+	assignee_group_id integer REFERENCES task_assignee_groups,
+	approver integer REFERENCES users,
 	
 	product_id integer REFERENCES products,
 	count integer,
@@ -70,16 +95,10 @@ create table tasks (
 	owner_id integer not null REFERENCES users,
 	updated_by integer REFERENCES users,
 	
-	assignee integer,
-	assigned_to_group boolean
 );
 
-create table task_assignee_groups (
-	id serial primary key,
-	task_id integer REFERENCES tasks
-	assignee_id integer not null REFERENCES users,
-	created_at timestamp without time zone NOT NULL,
-	owner_id integer not null REFERENCES users
+create table task_results (
+	
 );
 
 create table images (
